484ef84011e222340cad301745521b16d4a33d5d
Leandro <play3rtox1c@gmail.com>
2022-05-05 16:22:40 -0300

    add: finishing login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,24 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { AiOutlineInfoCircle } from 'react-icons/ai';
+import axios from 'axios';
 import '../css/login.css';
+
+const URL = process.env.REACT_APP_SERVER;
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isButtonActive, setIsButtonActive] = useState(false);
+  const [isInvalidLogin, setIsInvalidLogin] = useState(false);
   const navigate = useNavigate();
+
+  const showInvalidLoginMessage = () => {
+    setIsInvalidLogin(true);
+    setTimeout(() => {
+      setIsInvalidLogin(false)
+    }, 5000);
+  }
 
   const checkFields = () => {
     const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
@@ -16,9 +28,14 @@ const Login = () => {
     setIsButtonActive(isFieldsValid);
   }
 
-  const makeRequest = () => {
-    
-    navigate('/home');
+  const makeRequest = async () => {
+    await axios.post(URL + '/login', { email, password })
+      .then(() => {
+        navigate('/home');
+      })
+      .catch((err) => {
+        showInvalidLoginMessage()
+      })
   }
 
   useEffect(() => {
@@ -28,18 +45,33 @@ const Login = () => {
   return (
     <div className="login-page">
       <div className="login-container">
-        <h2>Login</h2>
-        <p>Email:</p>
-        <input 
-          onChange={ ({ target: { value } }) => setEmail(value) } 
-          value={ email } 
-        />
-        <p>Password:</p>
-        <input 
-          onChange={ ({ target: { value } }) => setPassword(value) } 
-          value={ password } 
-        />
+        <h2 className="hero-title login-title">Login</h2>
+        <div>
+          <p>Email:</p>
+          <input
+            placeholder="Digite seu Email..."
+            className="text-input"
+            onChange={ ({ target: { value } }) => setEmail(value) } 
+            value={ email } 
+          />
+        </div>
+        <div>
+          <p>Password:</p>
+          <input
+            placeholder="Digite sua senha..."
+            className="text-input"
+            type="password"
+            onChange={ ({ target: { value } }) => setPassword(value) } 
+            value={ password } 
+          />
+        </div>
+        { isInvalidLogin && 
+          <p>
+            <AiOutlineInfoCircle /> Invalid email or password!
+          </p> 
+        }
         <button
+          className="bot"
           onClick={ makeRequest }
           disabled={ !isButtonActive }
         >
